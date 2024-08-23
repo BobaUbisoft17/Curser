059aebb9c55a5708144d69eb11731bcdd5e1f353
BobaUbisoft17 <aksud2316@gmail.com>
2024-08-24 00:37:31 +0300

    Added config file

---
+++ api/config.py
@@ -0,0 +1,10 @@
+"""File for load config data."""
+
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    SECRETKEY: str = "SECRETKEY"
+    DBURL: str = "sqlite+aiosqlite:///sql_app.db"
+    JWTALGORITHM: str = ""
+    
