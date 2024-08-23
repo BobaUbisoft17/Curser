9d408aaaaf0a190295cfeb72da5208fc0d2ee4ad
BobaUbisoft17 <aksud2316@gmail.com>
2024-08-24 00:34:54 +0300

    flake8

--- api/database.py
+++ api/database.py
@@ -1,13 +1,18 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+"""File for connect with database."""
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
 
+
 class Database:
-    def __init__(self, db_url) -> None:
+    def __init__(self, db_url: str) -> None:
         self.engine = create_async_engine(
             db_url, connect_args={"check_same_thread": False}
         )
-        self.session = async_sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        self.session = async_sessionmaker(
+            autocommit=False, autoflush=False, bind=self.engine
+        )
