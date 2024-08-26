01bf3227130b6a916ede52ed6fa47b9c8320153a
BobaUbisoft17 <aksud2316@gmail.com>
2024-08-26 22:56:17 +0300

    Added s3 service's fields

--- api/config.py
+++ api/config.py
@@ -7,4 +7,8 @@ class Config(BaseSettings):
     SECRETKEY: str = "SECRETKEY"
     DBURL: str = "sqlite+aiosqlite:///sql_app.db"
     JWTALGORITHM: str = ""
+    BUCKETNAME: str = "test.bucket"
+    ENDPOINT: str = "http://localhost:9000"
+    ACCESS_KEY: str = "test_access_key"
+    SECRETKEYS3: str = "test_password"
     
