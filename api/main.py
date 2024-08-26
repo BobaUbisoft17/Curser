06ce193d8e409de755dfceae49da96a291fefaaf
BobaUbisoft17 <aksud2316@gmail.com>
2024-08-26 22:57:01 +0300

    Include s3 service middleware in application

--- api/main.py
+++ api/main.py
@@ -7,17 +7,27 @@ from .auth.router import router as auth_router
 from .auth.token_service import AuthJWT
 from .config import Config
 from .database import Database
-from .middlewares import DatabaseMiddleware, JWTMiddleware
+from .middlewares import DatabaseMiddleware, JWTMiddleware, S3Middleware
 from .profile.router import router as profile_router
+from ..s3.minio import S3Service
 
 
 class Memourse:
     def __init__(self) -> None:
         config = Config(_env_file=".env")
+
         db = Database(config.DBURL)
         auth_jwt = AuthJWT(config.SECRETKEY, config.JWTALGORITHM)
+        s3_service = S3Service(
+            config.BUCKETNAME,
+            config.ENDPOINT,
+            config.ACCESS_KEY,
+            config.SECRETKEYS3
+        )
+
         self.db_middleware = DatabaseMiddleware(db.session)
         self.jwt_middlware = JWTMiddleware(auth_jwt)
+        self.s3_middleware = S3Middleware(s3_service)
 
     def create_app(self) -> FastAPI:
         app = FastAPI()
@@ -25,6 +35,7 @@ class Memourse:
         app.include_router(profile_router)
         app.add_middleware(BaseHTTPMiddleware, dispatch=self.db_middleware)
         app.add_middleware(BaseHTTPMiddleware, dispatch=self.jwt_middlware)
+        app.add_middleware(BaseHTTPMiddleware, dispatch=self.s3_middleware)
         return app
 
 
