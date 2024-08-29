1cc1cff966b3ac0e525c024efd8bf4757906ef5c
BobaUbisoft17 <aksud2316@gmail.com>
2024-08-29 23:21:48 +0300

    Added excetions for update user data

--- api/profile/exceptions.py
+++ api/profile/exceptions.py
@@ -5,3 +5,15 @@ UserDoesNotExist = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND,
     detail="User does not exist"
 )
+
+
+UsernameIsTaken = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="Username is taken"
+)
+
+
+EmailIsTaken = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="Email is taken"
+)
