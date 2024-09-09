7c4793e79fd7e2152aa30e55f481373288c7b13a
BobaUbisoft17 <aksud2316@gmail.com>
2024-09-09 09:33:52 +0300

    Added base database url

--- api/config.py
+++ api/config.py
@@ -6,9 +6,5 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     SECRETKEY: str = "SECRETKEY"
     DBURL: str = "sqlite+aiosqlite:///sql_app.db"
-    JWTALGORITHM: str = ""
-    BUCKETNAME: str = "test.bucket"
-    ENDPOINT: str = "http://localhost:9000"
-    ACCESS_KEY: str = "test_access_key"
-    SECRETKEYS3: str = "test_password"
+    JWTALGORITHM: str = "HSA256"
     
